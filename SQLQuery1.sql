f257ac64d15a9ff8408f858b9f50bb71b293ed92
WuTsunglun <wutsunglun@outlook.com>
2017-04-25 11:35:40 +0800

    Frist checkin

---
+++ SQLQuery1.sql
@@ -0,0 +1,15 @@
+SELECT*
+FROM dbo.Categories
+WHERE CategoryID =2
+
+SELECT*
+FROM dbo.Products
+WHERE UnitsInStock =0
+
+SELECT*
+FROM dbo.Customers
+WHERE ContactTitle ='Marketing Manager'
+
+SELECT*
+FROM dbo.Customers
+WHERE Country ='Germany'
